49afc902157fc97cb18df21badbc398bcacd12f4
David Sanborn <dsanborn@loftware.com>
2024-06-17 14:18:52 -0400

    ng-drag-drop

--- index.ts
+++ index.ts
@@ -1,2 +1,4 @@
 export { NgDragDropModule } from './src/ng-drag-drop.module';
 export { DropEvent } from './src/shared/drop-event.model';
+export { Draggable } from './src/directives/draggable.directive'
+export { Droppable } from './src/directives/droppable.directive'
